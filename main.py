a9ecf9f23b821ea62bdacd981ece74bdbf438583
mark-brackenrig <37725651+mark-brackenrig@users.noreply.github.com>
2021-03-12 12:41:00 +1100

    prediction for single style

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from joblib import load
 import pandas as pd
 import torch
 import pickle as pkl
+from pydantic import BaseModel
+from src.models.predict_model import create_dataset, predict
+from src.models.pytorch import PytorchMultiClass,get_device
 
 ohe = pkl.load(open('models/ohe.pkl','rb'))
+scaler = pkl.load(open('models/scaler.pkl','rb'))
 y_encoder = pkl.load(open('models/y_encoder.pkl','rb'))
-model = torch.load('models/beer_style_prediction.pt')
+
+model = PytorchMultiClass(num_features = 1005,class_num= 104)
+model.load_state_dict(torch.load('models/beer_style_prediction.pt'))
 
 app = FastAPI()
 
@@ -21,13 +27,70 @@ def display_project_description():
 
 @app.get('/health/')
 def hello_world():
+    """
+    Health check for the API to determine that the API is running. Expected response is 'Hello world! App running.'
+
+    Parameters
+    ----------
+    None
+
+    Responses
+    ---------
+    200: 
+
+    """
     print("Hello World")
     return {"Hello World! App running."}
-    
-@app.post('/beer/type/')
-def predict_beer_type():
-    print("Hello World")
-    return {"Hello World! App running."}
+   
+class beerType(BaseModel):
+    """
+    The request model for the API to return a single prediciton
+    """
+    review_overall: float
+    review_aroma: float
+    review_appearance: float
+    review_palate: float
+    review_taste: float
+    brewery_names: str
+    class Config:
+        schema_extra = {
+            'example': {
+            "review_overall": 5,
+            "review_aroma": 5,
+            "review_appearance": 4,
+            "review_palate": 3,
+            "review_taste": 5,
+            "brewery_names": "Vecchio Birraio"
+            }
+        }
+class beerTypeOut(BaseModel):
+    class_name: list
+
+    class Config:
+        schema_extra = {
+            "example":{
+        "class_name": ["American IPA"]}
+        }
+
+@app.post('/beer/type/', response_model = beerTypeOut)
+def predict_beer_type(request_body: beerType):
+    """
+    Returns a single beer style prediction.
+
+    Parameters
+    ----------
+    * **review_overall** (float): Overall score given by reviewer - ranges from 1 to 5.
+    * **review_aroma** (float): Score given by reviewer regarding beer aroma- ranges from 1 to 5.
+    * **review_appearance** (float): Score given by reviewer regarding beer appearance - ranges from 1 to 5.
+    * **review_palate** (float): Score given by reviewer regarding beer palate- ranges from 1 to 5.
+    * **review_taste** (float): Score given by reviewer regarding beer taste - ranges from 1 to 5.
+    * **brwery_name** (string): Name of brewery - ranges from 1 to 5.
+    """
+    data = create_dataset(request_body,ohe,scaler)
+    device = get_device()
+    prediction = predict(data,model, device,y_encoder)
+    print(prediction[0])
+    return prediction[0]
 
 @app.post('/beers/type/')
 def predict_beers_type():
--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,15 +5,29 @@ from torch.utils.data import DataLoader
 import numpy as np
 import torch.nn.functional as F
 from src.data import make_dataset
+import pandas as pd
+from src.models.pytorch import get_device, PytorchDataset
 
 def create_dataset(request_body, ohe, scaler):
+    """
+    Converts the request body of the API to a numpy array that can be read by the model.
+    Parameters
+    ----------
+    request_body: dict
+    the request body from the API 
+    """
     X = pd.DataFrame(request_body)
-    brewery_names = ohe.transform(X['brewery_names'])
-    X.drop('brewery_names',inplace = True)
-    X = scaler.fit_transform(X)
+    X.columns = ['key','value']
+    X.set_index('key', inplace = True)
+    X = X.transpose().reset_index(drop=True)
+    val = np.array(X['brewery_names']).reshape(-1,1)
+    brewery_names = ohe.transform(val)
+    X.drop(columns = 'brewery_names',inplace = True, axis = 1)
+    X = scaler.transform(X)
     X = np.concatenate([X,brewery_names],axis = 1)
+    return X
 
-def predict(test_data, model,y_encoder, criterion, batch_size, device, generate_batch=None):
+def predict(test_data, model, device,y_encoder, generate_batch=None):
     """Calculate performance of a Pytorch multi-class classification model
 
     Parameters
@@ -33,18 +47,19 @@ def predict(test_data, model,y_encoder, criterion, batch_size, device, generate_
 
     Returns
     -------
-    Dictionary
-        Prediction: index of output
-        
+    Float
+        Loss score
+    Float:
+        Accuracy Score
     """    
-    
     # Set model to evaluation mode
     model.eval()
     test_loss = 0
     test_acc = 0
     
+    X = torch.Tensor(test_data)
     # Create data loader
-    data = DataLoader(test_data, batch_size=batch_size, collate_fn=generate_batch)
+    data = DataLoader(X, batch_size=1, collate_fn=generate_batch)
     results = []
     # Iterate through data by batch of observations
     for feature in data:
@@ -55,9 +70,10 @@ def predict(test_data, model,y_encoder, criterion, batch_size, device, generate_
         with torch.no_grad():
             # Make predictions
             output = model(feature)
-        
-        class_name = make_dataset.inverse_transformer(y,y_encoder)
-
-        results.append({'index': output.argmax(1)[0], 'class': class_name})
+        print(output)
+        prediction = output.argmax(1)[0].item()
+        class_name = make_dataset.inverse_transformer(np.array(prediction).reshape(-1,1),y_encoder) 
+        print(class_name.flatten())
+        results.append({'class_name':class_name.flatten().tolist()})
     return results
 
